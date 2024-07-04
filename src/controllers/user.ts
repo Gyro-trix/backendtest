fb5d0a79c71076ab40814ad50ab50cb3d33aa5e8
Matthew Garrett <m.garrett@hotmail.ca>
2024-07-04 11:26:54 -0230

    Finally got unit testing done for user.ts next authentication

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -4,6 +4,7 @@ import Joi from 'joi'
 
 import mysql, { RowDataPacket } from 'mysql2'
 import * as dotenv from 'dotenv'
+
 dotenv.config()
 
 const pool = mysql.createPool({
@@ -22,13 +23,8 @@ export interface User extends RowDataPacket{
   place: string
   bio: string
 }
-/*
-export interface Database{
-  create()
-  get(id: number): Promise<T>
-}
-*/
 
+//Joi min and max 2 for place
 const UserSchema = Joi.object<User>({
   id: Joi.number().optional(),
   name: Joi.string().required(),
@@ -72,7 +68,7 @@ export async function createUser(req:Request, res:Response){
     VALUES(?,?,?)
     `, [email,name,place])
 
-  return res.status(200).json(rest.success("User "+name+" created"))
+  return res.status(200).json(rest.success(name))
 
 }
 
@@ -119,24 +115,36 @@ export async function updateUser(req:Request, res:Response){
   const place = user.place
   const bio = user.bio
 
-  const [row] = await pool.query(`
-    UPDATE users
-    SET email = ?, name = ?, place = ?, bio = ?
-    WHERE id = ?
-    `, [email,name,place,bio,id])
+  const[exists] = await pool.query(`
+    SELECT * FROM users
+    WHERE email = ?
+    `,[email])
 
-  //If there are empty results, the ID is not in the table
-  const values = Object.values(row)
-  if (values.length === 0){
-    return res.status(400).json(rest.error('No Entry for given ID'))
-  }
+  const valueCheck = Object.values(exists)
+  if (valueCheck.length === 1){
+    return res.status(400).json(rest.error('Email already in use'))
+  } 
 
   const [result] = await pool.query(`
     SELECT * FROM users
     WHERE id = ?
     `, [id])
 
-  return res.status(200).json(rest.success(result))
+  console.log(Object.values(result).length)
+  //If there are empty results, the ID is not in the table
+  const values = Object.values(result)
+
+  if (values.length === 0){
+    return res.status(400).json(rest.error('No Entry for given ID'))
+  }
+
+  const [row] = await pool.query(`
+    UPDATE users
+    SET email = ?, name = ?, place = ?, bio = ?
+    WHERE id = ?
+    `, [email,name,place,bio,id])
+
+  return res.status(200).json(rest.success(row))
 
 }
 
@@ -151,6 +159,8 @@ export async function deleteUser(req:Request, res:Response){
     DELETE FROM users
     WHERE id = ?
     `, [id])
+
+  console.log(row)
 
   //If there are empty results, the ID is not in the table
   const values = Object.values(row)
--- test/controllers/user.test.ts
+++ test/controllers/user.test.ts
@@ -1,107 +1,278 @@
-import * as UserController from '../../src/controllers/user'
+import * as rest from '../../src/utils/rest'
 import { Request, Response } from 'express';
+import { getUser, createUser, updateUser, deleteUser} from '../../src/controllers/user'
+import mysql from 'mysql2'
 
-
-
-
-/*
-const mockRequest = (body: any) => {
-    return {
-        body: body,
-    } as unknown as Request;
-};
-
-const mockRequestParam = (params: any) => {
-    return {
-        params: params,
-    } as unknown as Request;
-};
-
-
-const mockResponse = () => {
-    let res = {
-        status: jest.fn(),
-        json: jest.fn()
+jest.mock('mysql2', () => {
+    const mockPool ={
+        query: jest.fn(),
+        promise: jest.fn().mockReturnThis(),
     };
-    res.status.mockReturnValue(res);
-    res.json.mockReturnValue(res);
-    return res as unknown as Response;
-};
+    return{
+        createPool: jest.fn((config)=>mockPool)
+    };
+});
 
-describe('createUser', () => {
-    beforeEach(() => {
-        jest.clearAllMocks()
-    })
+jest.mock('../../src/utils/rest', () => ({
+    error: jest.fn((msg:string)=>({error: msg})),
+    success: jest.fn((data: any)=>({data})),
+}))
 
-    it('Should create user and return 200', async() => {
-        let req = mockRequest({ name: 'John Doe', email: 'john2@doe.ca', place:'NL' });
-        let res = mockResponse();
-        
-        const id = Math.floor(0.5 * 1000000)
-        jest.spyOn(Math, 'random').mockReturnValue(0.5);
+describe('createUser', ()=>{
+    let req: Partial<Request>;
+    let res: Partial<Response>;
+    const mockPool = mysql.createPool({
+        host:'localhost',
+        user:'user',
+        password:'password',
+        database:'databse',
+    });
 
-        await UserController.createUser(req, res);
-        expect(res.status).toHaveBeenCalledWith(200)
-        expect(res.json).toHaveBeenCalledWith({
-            status: "success",
-            data: "User John Doe created"
-        })
-    })
+    beforeEach(()=>{
+        req = {
+            body:{
+                name: "test test",
+                email: "test@test.com",
+                place: "NL",
+                bio: "Testing so database stuff",
+            },
+        };
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+    });
 
-    it('should not create user and return 400 if no email is specified in the request', async() => {
-        let req = mockRequest({ name: 'John Doe' });
-        let res = mockResponse();
+    afterEach(()=>{
+        jest.clearAllMocks();
+    });
 
-        await UserController.createUser(req, res);
-        expect(res.status).toHaveBeenCalledWith(400)
-        expect(res.json).toHaveBeenCalledWith({
-            status: "error",
-            message: "User data is not formatted correctly"
-        })
-    })
+    it('should return error if user data is not formatted correctly', async () => {
+        req.body = { name: 'test test' }; // Incomplete data to trigger validation error
+        await createUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ error: 'User data is not formatted correctly' });
+      });
+    
+      it('should return error if user ID is present in request body', async () => {
+        req.body.id = 1; // Adding ID to trigger the error
+        await createUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ error: 'User ID will be generated automatically' });
+      });
+    
+      it('should return error if email already exists in the database', async () => {
+        (mockPool.query as jest.Mock).mockResolvedValueOnce([[{ id: 1, name: 'Test Bob', email: 'test@test.com', place: 'TS', bio: 'test' }]]);
+    
+        await createUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ error: 'Email already in use' });
+      });
+    
+      it('should create user and return success if data is valid and email is unique', async () => {
+        (mockPool.query as jest.Mock)
+          .mockResolvedValueOnce([[]]) // No existing user with the email
+          .mockResolvedValueOnce([{ insertId: 1 }]); // Simulate successful insert
+    
+        await createUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ data: 'test test' });
+      });
+    
 })
 
-describe('updateUser', () => {
-    beforeEach(() => {
-        jest.clearAllMocks()
-    })
+describe('getUser', ()=>{
+    let req: Partial<Request>;
+    let res: Partial<Response>;
+    const mockPool = mysql.createPool({
+        host:'localhost',
+        user:'user',
+        password:'password',
+        database:'databse',
+    });
 
-    it('should update user with given id and return 200', async() => {
-        let req = mockRequestParam({id:1 });
-        let reqBody = mockRequest({name: 'Bob', email: 'bob@bob.com', place:'NL',bio:'Testing it out'})
-        let res = mockResponse();
-        
-        const id = parseInt(req.params.id)
-        const name = reqBody.body.name
-        const email = reqBody.body.email
-        const place = reqBody.body.place
-        const bio = reqBody.body.bio
+    beforeEach(()=>{
+        req = {
+            params:{
+                id: '1',
+            },
+        };
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+    });
 
-        await UserController.updateUser(req, res);
-        expect(res.status).toHaveBeenCalledWith(200)
-        expect(res.json).toHaveBeenCalledWith({
-            status: "success",
-            data: {
-                email: email,
-                id: id,
-                name: name,
-                place: place,
-                bio: bio
+    afterEach(()=>{
+        jest.clearAllMocks();
+    });
+
+    it('should return error if ID is not a number', async () => {
+        req.params!.id = 'abc';
+        await getUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ error: 'ID is not a number' });
+      });
+
+    it('should return error if no entry for given ID', async () => {
+        (mockPool.query as jest.Mock).mockResolvedValueOnce([[]]);
+    
+        await getUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ error: 'No Entry for given ID' });
+      });
+    
+    it('should return user data if ID exists', async () => {
+        const mockUser = [{ id: 1, name: 'John Doe', email: 'john@example.com', place: 'NY', bio: 'Bio' }];
+        (mockPool.query as jest.Mock).mockResolvedValueOnce([mockUser]);
+    
+        await getUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ data: mockUser });
+      });
+})
+
+describe('updateUser', ()=>{
+    let req: Partial<Request>;
+    let res: Partial<Response>;
+    const mockPool = mysql.createPool({
+        host:'localhost',
+        user:'user',
+        password:'password',
+        database:'databse',
+    });
+
+    beforeEach(()=>{
+        req = {
+            body:{
+                name: "test test",
+                email: "test@test.com",
+                place: "NL",
+                bio: "Testing so database stuff",
+            },
+            params:{
+                id: '1'
             }
-        })
+        };
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+    });
+
+    afterEach(()=>{
+        jest.clearAllMocks();
+    });
+
+    it('should return error if user data is not formatted correctly', async () => {
+        req.body = { name: 'Test Bob' }; // Incomplete data to trigger validation error
+        await updateUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ error: 'User data is not formatted correctly' });
+      });
+    
+      it('should return error if ID is not a number', async () => {
+        req.params!.id = 'abc'; // Invalid ID to trigger the error
+        await updateUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ error: 'ID is not a number' });
+      });
+    
+      it('should return error if email already exists in the database', async () => {
+        (mockPool.query as jest.Mock).mockResolvedValueOnce([[{ id: 1, name: 'Test Test', email: 'test@test.com', place: 'TS', bio: 'Still testing' }]]);
+    
+        await updateUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ error: 'Email already in use' });
+      });
+    
+      it('should return error if no entry for the given ID', async () => {
+        (mockPool.query as jest.Mock)
+          .mockResolvedValueOnce([[]]) // No existing user with the email
+          .mockResolvedValueOnce([[]]); // Simulate no update due to ID not existing
+    
+        await updateUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ error: 'No Entry for given ID' });
+      });
+    
+      it('should update user and return success if data is valid and email is unique', async () => {
+        (mockPool.query as jest.Mock)
+          .mockResolvedValueOnce([[]]) // No existing user with the email
+          .mockResolvedValueOnce([{ affectedRows: 1 }]) // Simulate successful update
+          .mockResolvedValueOnce([[{ id: 1, name: 'Bob Tester', email: 'bob@tester.com', place: 'NL', bio: 'Bio' }]]); // Simulate fetching the updated user
+    
+        await updateUser(req as Request, res as Response);
+    
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ data: [{ id: 1, name: 'Bob Tester', email: 'bob@tester.com', place: 'NL', bio: 'Bio' }] });
+      });
+    })
+    describe('deleteUser', ()=>{
+        let req: Partial<Request>;
+        let res: Partial<Response>;
+        const mockPool = mysql.createPool({
+            host:'localhost',
+            user:'user',
+            password:'password',
+            database:'databse',
+        });
+    
+        beforeEach(()=>{
+            req = {
+                params:{
+                    id: '1'
+                }
+            };
+            res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
+        });
+
+        afterEach(()=>{
+            jest.clearAllMocks();
+        });
+
+        it('should return error if ID is not a number', async () => {
+            req.params!.id = 'abc'; // Invalid ID to trigger the error
+            await deleteUser(req as Request, res as Response);
+        
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ error: 'ID is not a number' });
+          });
+
+        it('should return error if no entry for the given ID', async () => {
+            (mockPool.query as jest.Mock)
+              .mockResolvedValueOnce([[]]); // Simulate no update due to ID not existing
+        
+            await deleteUser(req as Request, res as Response);
+        
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ error: 'No Entry for given ID' });
+        });
+
+        it('should delete user and return success if data is valid and email is unique', async () => {
+            (mockPool.query as jest.Mock)
+              .mockResolvedValueOnce([{ affectedRows: 1 }]) // Simulate successful update
+              //.mockResolvedValueOnce([[{ id: 1, name: 'Bob Tester', email: 'bob@tester.com', place: 'NL', bio: 'Bio' }]]); // Simulate fetching the updated user
+        
+            await deleteUser(req as Request, res as Response);
+        
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.json).toHaveBeenCalledWith({ data: { affectedRows: 1 } });
+          });
+
     })
 
-    it('should not update user and return 400 if no id is specified in the request', async() => {
-        let req = mockRequest({ name: 'John Doe',email:'john@doe.ca' });
-        let res = mockResponse();
-
-        await UserController.updateUser(req, res);
-        expect(res.status).toHaveBeenCalledWith(400)
-        expect(res.json).toHaveBeenCalledWith({
-            status: "error",
-            message: "Invalid user ID"
-        })
-    })
-
-})
-*/
